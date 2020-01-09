4c875b62e1ffa16e8cd8794ce26af45ea7128130
rOluochKe <rasholuoch@gmail.com>
2020-01-09 12:21:55 +0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
 
   def show
     @user = User.find(params[:id])
@@ -14,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Toy App!"
       redirect_to @user
     else
@@ -23,8 +21,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
